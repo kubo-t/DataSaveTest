b999215bb64e1727e658a486f81beab9c79e6e9f
kubotakuya <kbtk8427@gmail.com>
2018-11-11 20:25:38 +0900

    SaveData.csのテスト用です(11/11)

---
+++ Assets/Test2/Charactor.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Charactor : MonoBehaviour {
+
+	public string charactorId;
+	public int HP;
+
+	[System.Serializable]
+	public class Character
+	{
+		public string charactorId;
+		public int HP;
+
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+		Character charactor = new Character ();
+		charactor.charactorId = this.charactorId;
+		charactor.HP = this.HP;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Test2/GameCtrl.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameCtrl : MonoBehaviour {
+
+	public List<Charactor> ownCharactors = new List<Charactor>();
+
+	public void Save(){
+
+		SaveData.SetList ("ownCharactors", this.ownCharactors);
+
+		SaveData.Save ();
+
+	}
+
+	public void Load(){
+		
+		ownCharactors = SaveData.GetList ("ownCharactors", new List<Charactor>());
+
+		Debug.Log (this.ownCharactors[0].HP);
+		Debug.Log (this.ownCharactors[1].HP);
+		Debug.Log (this.ownCharactors[2].HP);
+		Debug.Log (this.ownCharactors[3].HP);
+		Debug.Log (this.ownCharactors[4].HP);
+	}
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Q)) {
+			Debug.Log ("Saved");
+			this.Save ();
+		}
+		if (Input.GetKeyDown (KeyCode.E)) {
+			Debug.Log ("Loaded");
+			this.Load ();
+		}
+	}
+
+
+}
